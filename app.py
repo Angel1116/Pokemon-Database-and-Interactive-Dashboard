0d66a44513a151c838cad47f853732f233f2225c
Fifi <fifi.ding@iss.nthu.edu.tw>
2024-12-13 13:49:37 +0800

    change leader image path

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ merged_df = pd.merge(gym_df, gym_pokemon_df, on='gym_id')
 merged_df = pd.merge(merged_df, df, left_on='pokemon', right_on='name')
 
 # Create a new column for the gym leader image paths, ensuring the leader names are in lowercase
-merged_df['leader_image_path'] = merged_df.apply(lambda row: f"assets/gen1_leaders/{row['location'].capitalize()}_{row['leader'].lower()}.png", axis=1)
+merged_df['leader_image_path'] = merged_df.apply(lambda row: f"assets/gen1_leaders/{row['leader'].lower()}.png", axis=1)
 
 # Create a new column for the Pokémon image paths, ensuring the Pokémon names are in lowercase
 merged_df['pokemon_image_path'] = merged_df['pokemon'].apply(lambda x: f"assets/images/{x.lower()}.png")
