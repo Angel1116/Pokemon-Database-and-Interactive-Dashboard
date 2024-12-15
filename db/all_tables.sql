9bfaaa3b58357381829eb98208ab434c819c0c02
Angel1116 <103301338+Angel1116@users.noreply.github.com>
2024-12-15 21:54:30 +0800

    Delete db/all_tables.sql

--- db/all_tables.sql
+++
@@ -1,7 +0,0 @@
-SELECT * 
-FROM Gym JOIN Gym_Pokemon ON (Gym.gym_id = Gym_Pokemon.gym_id)
-						JOIN Pokemon_Info ON (Gym_Pokemon.generation = Pokemon_Info.pokemon_generation
-																AND Gym_Pokemon.pokemon = Pokemon_Info.name)
-						JOIN Pokemon_Types ON (Pokemon_Info.pokemon_generation = Pokemon_Types.pokemon_generation
-																AND Pokemon_Info.name = Pokemon_Types.name)
-						JOIN Type_Effectiveness ON (Pokemon_Types.type = Type_Effectiveness.type)
